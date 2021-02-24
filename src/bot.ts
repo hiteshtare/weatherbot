0e3b704d5f3f469425757a2bafdc891faf45d2ce
Hitesh Tare <hiteshtare@gmail.com>
2021-02-24 16:42:31 +0530

    Implemented fetching of Weather using coordinates and then display Adaptive card

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,12 @@
 // Import node modules
 import { Logger } from "log4js";
+import request = require('request-promise');
+import * as moment from 'moment';
 
 // Import bot services
 import { BotAdapter, TurnContext, ActivityTypes, CardFactory } from "botbuilder";
 
+import { APP_CONFIG } from './config';
 export class WeatherBot {
   private _adapter: BotAdapter;
   private _logger: Logger;
@@ -25,89 +28,11 @@ export class WeatherBot {
 
       this._logger.warn(`sending... Weather Card`);
 
-      const cardInfo = {
-        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-        "type": "AdaptiveCard",
-        "version": "1.0",
-        "speak": "<s>The forecast for Seattle November 5 is mostly clear with a High of 50 degrees and Low of 41 degrees</s>",
-        "body": [
-          {
-            "type": "TextBlock",
-            "text": "Redmond, WA",
-            "size": "Large",
-            "isSubtle": true,
-            "wrap": true
-          },
-          {
-            "type": "TextBlock",
-            "text": "2019-11-05",
-            "spacing": "None",
-            "wrap": true
-          },
-          {
-            "type": "ColumnSet",
-            "columns": [
-              {
-                "type": "Column",
-                "width": "auto",
-                "items": [
-                  {
-                    "type": "Image",
-                    "url": "https://messagecardplayground.azurewebsites.net/assets/Mostly%20Cloudy-Square.png",
-                    "size": "Small"
-                  }
-                ]
-              },
-              {
-                "type": "Column",
-                "width": "auto",
-                "items": [
-                  {
-                    "type": "TextBlock",
-                    "text": "46",
-                    "size": "ExtraLarge",
-                    "spacing": "None",
-                    "wrap": true
-                  }
-                ]
-              },
-              {
-                "type": "Column",
-                "width": "stretch",
-                "items": [
-                  {
-                    "type": "TextBlock",
-                    "text": "°F",
-                    "weight": "Bolder",
-                    "spacing": "Small",
-                    "wrap": true
-                  }
-                ]
-              },
-              {
-                "type": "Column",
-                "width": "stretch",
-                "items": [
-                  {
-                    "type": "TextBlock",
-                    "text": "Hi 50",
-                    "wrap": true
-                  },
-                  {
-                    "type": "TextBlock",
-                    "text": "Lo 41",
-                    "spacing": "None",
-                    "wrap": true
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      };
+      const weatherCard = await this.getWeatherCardByCoordinates();
+
       //============================Adaptive Card============================
       await context.sendActivity({
-        attachments: [CardFactory.adaptiveCard(cardInfo)]
+        attachments: [CardFactory.adaptiveCard(weatherCard)]
       });
       //============================Adaptive Card============================
 
@@ -116,4 +41,128 @@ export class WeatherBot {
       this._logger.info(`EventType:${context.activity.type} is detected`);
     }
   }
+
+
+  async getWeatherCardByCoordinates() {
+    this._logger.warn(`getWeatherCardByCoordinates`);
+
+    const fetchedWeatherDetails = await this.fetchWeatherDetails();
+    this._logger.info(`Fetched Weather Details!`);
+    this._logger.debug(fetchedWeatherDetails);
+
+    const cardInfo = {
+      "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+      "type": "AdaptiveCard",
+      "version": "1.0",
+      "speak": `<s>The forecast for ${fetchedWeatherDetails.name} November 5 is ${fetchedWeatherDetails.weather[0].description} with a High of ${fetchedWeatherDetails.main.temp_max} degrees and Low of ${fetchedWeatherDetails.main.temp_min} degrees</s>`,
+      "body": [
+        {
+          "type": "TextBlock",
+          "text": `${fetchedWeatherDetails.name}, ${fetchedWeatherDetails.sys.country}`,
+          "size": "Large",
+          "isSubtle": true,
+          "wrap": true
+        },
+        {
+          "type": "TextBlock",
+          // "text": `${this.formatDateForCard('')}`,
+          "text": `Feb 24, 2021`,
+          "spacing": "None",
+          "wrap": true
+        },
+        {
+          "type": "ColumnSet",
+          "columns": [
+            {
+              "type": "Column",
+              "width": "auto",
+              "items": [
+                {
+                  "type": "Image",
+                  "url": `http://openweathermap.org/img/wn/${fetchedWeatherDetails.weather[0].icon}.png`,
+                  "size": "Small"
+                }
+              ]
+            },
+            {
+              "type": "Column",
+              "width": "auto",
+              "items": [
+                {
+                  "type": "TextBlock",
+                  "text": `${Math.round(+fetchedWeatherDetails.main.temp)}`,
+                  "size": "ExtraLarge",
+                  "spacing": "None",
+                  "wrap": true
+                }
+              ]
+            },
+            {
+              "type": "Column",
+              "width": "stretch",
+              "items": [
+                {
+                  "type": "TextBlock",
+                  "text": "°C",
+                  "weight": "Bolder",
+                  "spacing": "Small",
+                  "wrap": true
+                }
+              ]
+            },
+            {
+              "type": "Column",
+              "width": "stretch",
+              "items": [
+                {
+                  "type": "TextBlock",
+                  "text": `Hi ${fetchedWeatherDetails.main.temp_max}`,
+                  "wrap": true
+                },
+                {
+                  "type": "TextBlock",
+                  "text": `Lo ${fetchedWeatherDetails.main.temp_min}`,
+                  "spacing": "None",
+                  "wrap": true
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+
+    return cardInfo;
+  }
+
+  async fetchWeatherDetails() {
+    this._logger.info(`fetchWeatherDetails`);
+
+    const weatherDetailsUri = `https://api.openweathermap.org/data/2.5/weather?lat=${APP_CONFIG.latitude}&lon=${APP_CONFIG.longitude}&appid=${APP_CONFIG.openWeatherAPIKey}&units=metric`;
+    this._logger.debug(`weatherDetailsUri : ${weatherDetailsUri}`);
+
+    //GET Request for fetching GeneeConfig
+    const getOptions = {
+      method: 'GET',
+      uri: weatherDetailsUri,
+    };
+
+    return await request(getOptions)
+      .then((resp: any) => {
+        this._logger.info(`WeatherDetails fetched successfully.`);
+        return JSON.parse(resp);
+      }).catch((err) => {
+        this._logger.error(err);
+      });
+  }
+
+  async formatDateForCard(date: string) {
+    try {
+      return moment(date).format("MMM DD, YYYY").toString();
+    } catch (err) {
+      this._logger.error(err);
+      return '';
+    }
+  }
 }
+
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const APP_CONFIG = {
+  openWeatherAPIKey: 'eea95aa9eccc24a7e55d6ced4a3c098a',
+  latitude: "19.287140",
+  longitude: "72.868843"
+};
